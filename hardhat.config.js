f837d9ccd6b4f41a5b4302c167b0221ac3a0d276
Tanishka <tani321@gmail.com>
2025-11-23 00:51:50 +0530

    updated file

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,23 +2,22 @@
 require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
     },
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
+    sepolia: {
+      url: "https://ethereum-sepolia-rpc.publicnode.com",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 80001,
-      gasPrice: 35000000000, // 35 gwei
+      chainId: 11155111,
+      timeout: 60000,
     }
   },
   etherscan: {
     apiKey: {
-      polygonMumbai: process.env.POLYGONSCAN_API_KEY || ""
+      sepolia: process.env.ETHERSCAN_API_KEY || ""
     }
   }
 };
